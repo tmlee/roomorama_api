fa215675562ed84a7abcb2d43579299171916c21
TM Lee <tm89lee@gmail.com>
2013-03-14 23:18:38 +0800

    include stub helper for delete

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -32,6 +32,13 @@ def stub_post(url, filename, options={})
   FakeWeb.register_uri(:post, url, opts)
 end
 
+def stub_delete(url, filename, options={})
+  opts = {
+    :body => error_or_standard_body(filename, options),
+  }.merge(options)
+  FakeWeb.register_uri(:delete, url, opts)
+end
+
 
 def error_or_standard_body(filename, options)
   error_options = options.delete(:error)
